f6c6d29e6c6e3193b770b38ce9c749aa2f2e5de3
Anabella Spinelli <anabellaspinelli@gmail.com>
2019-06-20 12:29:57 +0200

    Added polyfill for selected browsers.

--- webpack.config.base.js
+++ webpack.config.base.js
@@ -15,7 +15,21 @@ module.exports = {
         loader: "babel-loader",
         exclude: /node_modules/,
         options: {
-          presets: ["@babel/preset-env", "@babel/preset-react"],
+          presets: [
+            [
+              "@babel/preset-env",
+              {
+                targets: [
+                  "last 2 versions",
+                  "not dead",
+                  "not < 2%",
+                  "not ie 11",
+                ],
+                useBuiltIns: "usage", // experimental, standard is "entry"
+              },
+            ],
+            "@babel/preset-react",
+          ],
           plugins: [
             "@babel/plugin-proposal-class-properties",
             "react-hot-loader/babel",
